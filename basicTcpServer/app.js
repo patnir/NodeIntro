c5e5adfd1464ff0fa8ebac3e3080c8d276b601ed
patnir <rahul20patni@gmail.com>
2016-08-06 19:34:27 -0400

    Basic tcp server

---
+++ basicTcpServer/app.js
@@ -0,0 +1,9 @@
+var net = require('net');
+
+var server = net.createServer(
+	function(socket) {
+		console.log("Connection from " + socket.remoteAddress);
+		socket.end("Hello World Zings\n");
+	});
+
+server.listen(7000, "127.0.0.1");
--- readFileSyncAsync/app.js
+++ readFileSyncAsync/app.js
@@ -8,3 +8,19 @@ fs.readFile('package.json',
 	function(err, buf) {
 		console.log(buf.toString());
 	});
+
+
+// Call back style programming
+
+/*
+	Event loops result in call back style programming when 
+	you break apart a program into its underlying dataflow
+*/
+
+// Promises
+
+/*
+	A funtion will return a promise for an object in the future
+	Promises can be chained together
+	Simplifying programming of async systems
+*/
