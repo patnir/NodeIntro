78ffa0a8df14646876fe0cdb9a524acd0b67cf82
patnir <rahul20patni@gmail.com>
2016-08-13 14:48:05 -0400

    Express app working

--- basicExpress/app.js
+++ basicExpress/app.js
@@ -1,35 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
-
 var routes = require('./routes');
-
 var user = require('./routes/user');
-
 var http = require('http');
 var path = require('path');
 
 var app = express();
 
+// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.json());
 
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 
-app.use(app.router);
-app.use(require('stylus')).middleware(path.join(__dirname, 'public'));
+var bodyParser = require('body-parser');
 
+var methodOverride = require('method-override');
+
+
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// development only
 if ('development' == app.get('env')) {
-	app.use(express.errorHandle());
+    var errorHande = require('errorhandler');
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
